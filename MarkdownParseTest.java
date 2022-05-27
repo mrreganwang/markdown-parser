618189d90b7b47f6bb0eaed496a171af6229c1e8
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-05-27 08:26:44 -0700

    added snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -29,4 +29,28 @@ public class MarkdownParseTest {
     //     String content = Files.readString(fileName);
     //     assertEquals(List.of("https://something.com", "some-thi)ng.html"), MarkdownParse.getLinks(content));
     // }
+    @Test
+    public void testSnippet1() throws IOException{
+        Path fileName = Path.of("snippet1.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("`google.com", "google.com", 
+        "ucsd.edu"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        Path fileName = Path.of("snippet2.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), 
+        MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        Path fileName = Path.of("snippet3.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("http://www.twitter,com", 
+        "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", 
+        "https://cse.ucsd.edu/"), MarkdownParse.getLinks(content));
+    }
 }
